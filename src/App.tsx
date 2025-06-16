1c1bcb7be4b19e1663fc5e208d57dbf4c00cc9bd
Yazan abomokh <yazanabomokh935@gmail.com>
2025-06-16 21:21:48 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,23 @@
-import {
-  ActionBar,
-  Button,
-  Checkbox,
-  ClientOnly,
-  CloseButton,
-  IconButton,
-  Portal,
-  Skeleton,
-} from "@chakra-ui/react";
-import {
-  ColorModeButton,
-  DarkMode,
-  LightMode,
-  useColorMode,
-  useColorModeValue,
-} from "@/components/ui/color-mode";
-import { useEffect, useState } from "react";
-import { LuMoon, LuShare, LuSun, LuTrash2 } from "react-icons/lu";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const { toggleColorMode, colorMode } = useColorMode();
-  const [checked, setChecked] = useState(false);
   return (
-    <>
-      <ClientOnly fallback={<Skeleton boxSize="8" />}>
-        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
-          {colorMode === "light" ? <LuSun /> : <LuMoon />}
-        </IconButton>
-      </ClientOnly>
-      <br />
-      <br />
-      <Checkbox.Root
-        checked={checked}
-        onCheckedChange={(e) => setChecked(!!e.checked)}
-      >
-        <Checkbox.HiddenInput />
-        <Checkbox.Control />
-        <Checkbox.Label>Show Action bar</Checkbox.Label>
-      </Checkbox.Root>
-
-      <ActionBar.Root
-        open={checked}
-        onOpenChange={(e) => setChecked(e.open)}
-        closeOnInteractOutside={false}
-      >
-        <Portal>
-          <ActionBar.Positioner>
-            <ActionBar.Content>
-              <ActionBar.SelectionTrigger>
-                2 selected
-              </ActionBar.SelectionTrigger>
-              <ActionBar.Separator />
-              <Button variant="outline" size="sm">
-                <LuTrash2 />
-                Delete
-              </Button>
-              <Button variant="outline" size="sm">
-                <LuShare />
-                Share
-              </Button>
-              <ActionBar.CloseTrigger asChild>
-                <CloseButton size="sm" />
-              </ActionBar.CloseTrigger>
-            </ActionBar.Content>
-          </ActionBar.Positioner>
-        </Portal>
-      </ActionBar.Root>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
